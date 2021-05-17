4f1d9500c19cb3a61b9e6737864a34ab25146a68
Jcsxabi14 <76202428+Jcsxabi14@users.noreply.github.com>
2021-05-17 09:20:38 +0200

    Update leerFicheroXML.js

--- ejercicio1/leerFicheroXML.js
+++ ejercicio1/leerFicheroXML.js
@@ -3,7 +3,7 @@ function gestionarFicheroXML(xmlDoc){
 	let libros = xmlDoc.querySelectorAll("libro")
 	for(let i=0; i<libros.length; i++)
 		if (i == 2) 
-		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='sub'>" + libros[i].textContent + "</p>"
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='sub borde'>" + libros[i].textContent + "</p>"
 		else
 			capaVacia.innerHTML = capaVacia.innerHTML + "<p class='borde'>" + libros[i].textContent + "</p>"
 		
