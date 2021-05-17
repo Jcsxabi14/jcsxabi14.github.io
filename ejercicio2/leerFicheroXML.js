252158d9ea0db7afb62f857dd664ecd972dfb344
Jcsxabi14 <76202428+Jcsxabi14@users.noreply.github.com>
2021-05-17 09:26:44 +0200

    Add files via upload

---
+++ ejercicio2/leerFicheroXML.js
@@ -0,0 +1,12 @@
+function gestionarFicheroXML(xmlDoc){
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("libro")
+	for(let i=0; i<libros.length; i++)
+		if(i i%2=0) 
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p class='par'>" + libros[i].textContent + "</p>"
+			else
+				capaVacia.innerHTML = capaVacia.innerHTML + "<p class='impar'>" + libros[i].textContent + "</p>"
+	
+}
+
+	loadDocA("libros.xml","xml");
