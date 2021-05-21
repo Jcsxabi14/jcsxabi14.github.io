810a5e5075f58193d7190e44ca6ab03bfe4398c1
Jcsxabi14 <76202428+Jcsxabi14@users.noreply.github.com>
2021-05-21 09:37:20 +0200

    Add files via upload

---
+++ Examen/leerFicheroXML.js
@@ -0,0 +1,11 @@
+function gestionarFicheroXML(xmlDoc)
+{
+	let capaVacia = document.querySelector("#ficheroXML")
+	let libros = xmlDoc.querySelectorAll("conver")
+}
+setInterval(CargarFichero, 2000)
+function CargarFichero()
+{
+ 	loadDocA("libros.xml","xml")
+}
+	
